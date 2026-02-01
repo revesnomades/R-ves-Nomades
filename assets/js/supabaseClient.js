4165f1ebeb6bb5800c8543cf555872fc1b866383
rêves-nomades <contact@revesnomades.com>
2026-02-02 00:10:14 +0100

    modif 01/02 3

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -5,7 +5,7 @@ const SUPABASE_URL = "https://qxrxwgrgjvesevjkjmry.supabase.co";
 
 // ⚠️ Mets ici ta clé ANON publique (Supabase > Settings > API > anon public)
 // Elle commence souvent par "eyJ..."
-const SUPABASE_ANON_KEY = "COLLE_ICI_TA_CLE_ANON";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4cnh3Z3JnanZlc2V2amtqbXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNzE4MTIsImV4cCI6MjA4Mjk0NzgxMn0.WQCWMfqGbhbdnYdkcpBAarVJBBstSFAK3_YYn5uNbKI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
